12db0d3617f2135c97acc3818974d9d3f5626ea3
EricdSouzx <eericdsn@gmail.com>
2025-08-11 11:34:25 -0300

    feat: adiciona primeira controller/regra para empresas

---
+++ src/controllers/empresaController.js
@@ -0,0 +1,29 @@
+const empresaModel = require("../models/empresaModel");
+
+function buscarCnpj(req, res) {
+    const cnpj = req.body.cnpj;
+    
+    empresaModel.buscarCnpj(cnpj).then((resultado) => {
+        res.status(200).json(resultado);
+    });
+}
+
+function cadastrar(req, res) {
+    const nome_empresa = req.body.nome_empresa;
+    const cnpj = req.body.cnpj;
+    const telefone = req.body.telefone;
+    const email = req.body.email;
+    const endereco = req.body.endereco;
+    const cidade = req.body.cidade;
+    const estado = req.body.estado;
+
+    empresaModel.buscarCnpj(cnpj).then((resultado) => {
+        if (resultado.length > 0) {
+            res.status(401).json({ mensagem: `A empresa com o CNPJ: ${cnpj} já existe` });
+        } else {
+            empresaModel.cadastrar(nome_empresa, cnpj, telefone, email, endereco, cidade, estado).then((resultado) => {
+                res.status(201).json(resultado);
+            });
+        }
+    });
+}
